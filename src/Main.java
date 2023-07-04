b8b40dc46671fce6933f668a8c5645565de7153d
Alexey <alexeyrueda823@gmail.com>
2023-07-03 20:45:11 -0500

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,12 @@
+import javax.swing.*;
+//Grupo de Cristian Jurado, Andre Rosero y Jair Rueda
+
+public class Main {
+    public static void main(String[] args) {
+        Interfaz b = new Interfaz();
+        b.setTitle("GRAFOS");
+        b.setSize(800, 700);
+        b.setVisible(true);
+        b.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    }
+}
